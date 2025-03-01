219ced3ab10cf1f4520af5517af807165bd3d667
Loddit Li <loddit@gmail.com>
2025-03-01 09:46:19 +0800

    setup github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://loddit.github.io',
   integrations: [react()],
 
   vite: {
